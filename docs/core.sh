7b7e108d8ab3e7a227aa4e99b60841968908cd1e
Nogeese <leon8326nogeese@outlook.com>
2025-03-17 14:56:56 +0100

    Create core.sh

---
+++ docs/core.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Docker Nogeese Server Essentials
+
+# Basic Info
+INFOTYPE=software
+INFOSOFTWARENAME=docker-nogeese-server-core
+INFOLATESTVERSION=1
+
+# Sources settings
+INFOINSTALLABLE=true
+INFOREINDERENABLE=false
+INFOINSTALLERENABLE=true
+INFOREPOENABLE=false
+INFOPACKAGEENABLE=false
+
+# Sources
+INFOINSTALLERSOURCE=https://raw.githubusercontent.com/nogeese-org/docker-server/main/installer.sh
+
+# Dependencies
+DEPENDENCIESENABLE=false

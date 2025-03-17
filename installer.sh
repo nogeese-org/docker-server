04c4616f67101b1d239cdc096ee18383ac8d3f6e
Nogeese <leon8326nogeese@outlook.com>
2025-03-17 16:14:41 +0100

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/sudo bash
+
+infostack -S https://nogeese-org.github.io/InfoKit/uinfostack/infostack.sh
+uinfostack -S https://nogeese-org.github.io/docker-server/shell/main/infostack.sh
+uinfostack -S https://nogeese-org.github.io/mircks/official/betterdockerserver/uinfostack.sh
